b25915daf24f22fb1c3479f8a7c0a88132a173e4
havr1985 <gavrtel1985@gmail.com>
2023-08-01 17:39:03 +0300

    ended job

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,15 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const container = document.querySelector(".gallery");
 
-const markupGallery = galleryItems.map(({ preview, description, original }) => `
+const markupGallery = galleryItems.map(({ preview, description, original }) => `<li>
    <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
-   </a>`).join('');
+   </a></li>`).join('');
 
 container.insertAdjacentHTML('beforeend', markupGallery);
-console.log(container);
 
-document.addEventListener('DOMContentLoaded', function () {
-      const gallery = new SimpleLightbox('.gallery');
-    });
+const gallery = new SimpleLightbox('.gallery a', {
+   captionsData: 'alt',
+   captionDelay: 250,
+});
+    
 
