6e802e712fbc9081a9bb13192bb741b81bc322f2
havr1985 <gavrtel1985@gmail.com>
2023-08-01 17:02:55 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markupGallery = galleryItems.map(({ preview, description, original }) => `
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>`).join('');
+
+container.insertAdjacentHTML('beforeend', markupGallery);
+console.log(container);
+
+document.addEventListener('DOMContentLoaded', function () {
+      const gallery = new SimpleLightbox('.gallery');
+    });
+
